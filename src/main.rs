4821c5ecfd8fc213dab66fa939185871a8a12959
Balázs Marcell <KoegLike@proton.me>
2025-06-26 09:52:01 +0000

    Refactor Cargo.toml dependencies and enhance main.rs with async task execution for LED blinking

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use embassy_executor::{Executor, Spawner};
+use embassy_time::{Duration, Timer};
+use esp_idf_svc::hal::{gpio::*, peripheral::*, prelude::Peripherals};
+use static_cell::StaticCell;
+
+static EXECUTOR: StaticCell<Executor> = StaticCell::new();
+
 fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
@@ -6,5 +13,37 @@ fn main() {
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    log::info!("Hello, world!");
+    log::info!("hali");
+
+    let executor = Executor::new();
+    let executor: &'static mut Executor = EXECUTOR.init(executor);
+
+    executor.run(|spawner| spawner.spawn(main_task(spawner)).unwrap());
+}
+
+#[embassy_executor::task]
+async fn main_task(spawner: Spawner) {
+    let p = Peripherals::take().unwrap();
+    let pin = p.pins.gpio12;
+    // Spawned tasks run in the background, concurrently.
+    log::info!("spawn");
+    spawner.spawn(blink(pin.into())).unwrap();
+    loop {
+        Timer::after_millis(150).await;
+        log::info!("11");
+    }
+}
+
+#[embassy_executor::task]
+async fn blink(pin: AnyOutputPin) {
+    let mut led = PinDriver::output(pin).unwrap();
+    loop {
+        // Timekeeping is globally available, no need to mess with hardware timers.
+        led.set_high().unwrap();
+        Timer::after_millis(150).await;
+        log::info!("high");
+        led.set_low().unwrap();
+        Timer::after_millis(150).await;
+        log::info!("low");
+    }
 }
