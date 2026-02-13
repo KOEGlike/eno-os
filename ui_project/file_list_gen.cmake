a99a3add52ce7647289c15bbc5e013560f6342c9
Balázs Marcell <KoegLike@proton.me>
2026-02-13 18:13:34 +0200

    Refactor UI project structure: replace Hello World module with new screens and initialization functions

--- ui_project/file_list_gen.cmake
+++ ui_project/file_list_gen.cmake
@@ -1,5 +1,6 @@
 list(
   APPEND
   LV_EDITOR_PROJECT_SOURCES
-  ${CMAKE_CURRENT_LIST_DIR}/ui_hello_world_gen.c
-  ${CMAKE_CURRENT_LIST_DIR}/ui_hello_world.c)
+  ${CMAKE_CURRENT_LIST_DIR}/screens/lol_gen.c
+  ${CMAKE_CURRENT_LIST_DIR}/ui_project_gen.c
+  ${CMAKE_CURRENT_LIST_DIR}/ui_project.c)
---
+++ ui_project/ui_project_gen.h
@@ -0,0 +1,89 @@
+/**
+ * @file ui_project_gen.h
+ */
+
+#ifndef UI_PROJECT_GEN_H
+#define UI_PROJECT_GEN_H
+
+#ifndef UI_SUBJECT_STRING_LENGTH
+#define UI_SUBJECT_STRING_LENGTH 256
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+
+#ifdef LV_LVGL_H_INCLUDE_SIMPLE
+    #include "lvgl.h"
+#else
+    #include "lvgl/lvgl.h"
+#endif
+
+
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL VARIABLES
+ **********************/
+
+/*-------------------
+ * Permanent screens
+ *------------------*/
+
+/*----------------
+ * Global styles
+ *----------------*/
+
+/*----------------
+ * Fonts
+ *----------------*/
+
+/*----------------
+ * Images
+ *----------------*/
+
+/*----------------
+ * Subjects
+ *----------------*/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+
+/*----------------
+ * Event Callbacks
+ *----------------*/
+
+/**
+ * Initialize the component library
+ */
+
+void ui_project_init_gen(const char * asset_path);
+
+/**********************
+ *      MACROS
+ **********************/
+
+/**********************
+ *   POST INCLUDES
+ **********************/
+
+/*Include all the widgets, components and screens of this library*/
+#include "screens/lol_gen.h"
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif /*UI_PROJECT_GEN_H*/
